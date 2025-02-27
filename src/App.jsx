9da2bd450b69c7f20555898c04514ad387434513
Sahil-Rawat-001 <rawatsahilwork001@gmail.com>
2025-02-27 13:04:00 +0530

    add ForgotPasswordPage component (frontend) with ninth commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 // import SignUpPage from "./SignUpPage/SignUpPage.jsx"
-import SignInPage from "./SignInPage/SignInPage.jsx"
+// import SignInPage from "./SignInPage/SignInPage.jsx"
+import ForgotPasswordPage from "./ForgotPasswordPage/ForgotPasswordPage"
 
 function App() {
   
 
   return (
     <>
-      <SignInPage/>
+      <ForgotPasswordPage/>
     </>
   )
 }
